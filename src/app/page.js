62a66a52349887f5bda481001ec425abafc0cf1f
chandro4D <chandroshakhor7@gmail.com>
2024-09-18 19:07:07 +0600

    add footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,21 @@
 import Navbar from "./components/common/Navbar";
-import Footer from "./components/common/Footer";
+
 import Banner from "./components/home/Banner";
 import Link from "next/link";
+import Footer from "./pages/footer/Footer";
 
 export default function Home() {
   return (
-    <div className="max-w-3xl mx-auto">
+    <div className=" mx-auto">
       <Navbar/>
       <Banner/>
-     <Footer/>
+     
       
       <div className="border p-6 flex items-center gap-5">
         <Link href="/pages/login">Login</Link>
         <Link href="/pages/registration">Registration</Link>
       </div>
+      <Footer></Footer>
     </div>
   );
 }
