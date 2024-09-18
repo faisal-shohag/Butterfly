f94272ee1564d323be9755402c6e7244fb6e70f7
Robiu-Sani <hafazrobiussani@gmail.com>
2024-09-18 15:36:40 +0600

    login banner is done

--- src/app/pages/login/page.js
+++ src/app/pages/login/page.js
@@ -1,10 +1,28 @@
+import Image from "next/image";
+import BgImage from "../../../_images/AuthBg.jpg";
 
 const Login = () => {
-    return (
-        <div>
-            Login
-        </div>
-    );
+  return (
+    <div className="w-full min-h-screen py-10 relative px-3 flex justify-center items-center">
+      <Image
+        src={BgImage}
+        alt="this is background image"
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+        className="z-0 absolute left-0"
+      />
+      <div
+        className="w-full h-full absolute left-0 bg-[#0a0a0a59]"
+        style={{
+          backgroundImage: `linear-gradient(90deg, #0a0a0ade, #0a0a0abd,#0a0a0abd, #0a0a0a56, #0a0a0a56)`,
+        }}
+      ></div>
+
+      {/* Content */}
+      <div className="relative z-100 text-white">Login</div>
+    </div>
+  );
 };
 
 export default Login;
