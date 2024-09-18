e381d94e0a32c963aa91959a29a06478fe0ed78f
faisal-shohag <faisalshohagprog@gmail.com>
2024-09-18 14:30:38 +0600

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/common/Navbar";
 import Footer from "./components/common/Footer";
 import Banner from "./components/home/Banner";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,6 +10,10 @@ export default function Home() {
       <Banner/>
      <Footer/>
       
+      <div className="border p-6 flex items-center gap-5">
+        <Link href="/pages/login">Login</Link>
+        <Link href="/pages/registration">Registration</Link>
+      </div>
     </div>
   );
 }
