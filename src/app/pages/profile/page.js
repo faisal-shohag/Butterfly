af83c65ed79c03784b07d47c6c5da52946bc9a76
Robiu-Sani <hafazrobiussani@gmail.com>
2024-09-24 16:39:02 +0600

    book exchange form is done

--- src/app/pages/profile/page.js
+++ src/app/pages/profile/page.js
@@ -1,19 +1,52 @@
-"use client"
+"use client";
 
 import Image from "next/image";
+import ExchangeForm from "./ExchangeForm";
 
 const Profile = () => {
-
   return (
     <div className="mx-auto mt-[-20px]">
-      <div style={{backgroundImage: "url('https://i.postimg.cc/05wQx63s/image.png')", backgroundPosition: 'bottom', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', borderBottomLeftRadius: '10px', borderBottomRightRadius: '10px'}} className="custom-glass-2 h-32 relative mb-28">
-      <div className="absolute bottom-[-70px] left-[10px] border-4 rounded-full dark:border-zinc-900 border-white">
-        <Image src="https://i.postimg.cc/5tqhtjwH/image.png" alt="profile" width={100} height={100} className="rounded-full" fit/>
+      <div
+        style={{
+          backgroundImage: "url('https://i.postimg.cc/05wQx63s/image.png')",
+          backgroundPosition: "bottom",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          borderBottomLeftRadius: "10px",
+          borderBottomRightRadius: "10px",
+        }}
+        className="custom-glass-2 h-32 relative mb-28"
+      >
+        <div className="absolute bottom-[-70px] left-[10px] border-4 rounded-full dark:border-zinc-900 border-white">
+          <Image
+            src="https://i.postimg.cc/5tqhtjwH/image.png"
+            alt="profile"
+            width={100}
+            height={100}
+            className="rounded-full"
+            fit
+          />
+        </div>
+        <div className="absolute left-32 -bottom-16">
+          <div className="text-2xl font-bold">Jabir Ibn Haiyan</div>
+          <div className="font-semibold">jabir.haiyan@gmail.com</div>
+        </div>
       </div>
-       <div className="absolute left-32 -bottom-16">
-        <div className='text-2xl font-bold'>Jabir Ibn Haiyan</div>
-        <div className="font-semibold">jabir.haiyan@gmail.com</div>
-       </div>
+      <div className="container mx-auto px-2">
+        <div className="custom-glass w-full p-2 mb-3 rounded-md">
+          <button className="custom-glass px-4 font-bold text-gray-600 border !py-1 mr-2 rounded-md ">
+            About you
+          </button>
+          <button className="custom-glass px-4 font-bold text-gray-600 border !py-1 mr-2 rounded-md ">
+            Add a Book
+          </button>
+          <button className="custom-glass px-4 font-bold text-gray-600 border !py-1 mr-2 rounded-md ">
+            Exchanges
+          </button>
+        </div>
+        <div className="custom-glass w-full p-2 rounded-md">
+          <ExchangeForm></ExchangeForm>
+        </div>
       </div>
     </div>
   );
