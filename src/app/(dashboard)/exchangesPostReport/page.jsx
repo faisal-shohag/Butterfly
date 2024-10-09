e6742244a6336431356fa359410e5846e1d9cfda
Masudur400 <md8004210@gmail.com>
2024-10-10 00:37:29 +0600

    report add done

--- src/app/(dashboard)/exchangesPostReport/page.jsx
+++ src/app/(dashboard)/exchangesPostReport/page.jsx
@@ -1,10 +1,28 @@
 'use server'
+import Chart from '@/components/Chart';
 import React from 'react';
 
 const page = () => {
+
+    const name = 'Total Exchange posts'
+    const amount = 'Total Exchange Post Users'
+
     return (
         <div>
-            Exchanges Post Report
+            <div>
+                <h3 className="text-2xl font-bold text-center p-3 border shadow-md rounded-sm">Exchanges Posts Report</h3>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-7'>
+                <div className='p-3 border shadow-md rounded-sm hover:shadow-lg hover:scale-105'>
+                    <h2 className="text-xl font-bold">Total Exchange posts</h2>
+                    <h2 className="text-2xl font-bold text-center">200</h2>
+                </div>
+                <div className='p-3 border shadow-md rounded-sm hover:shadow-lg hover:scale-105'>
+                    <h2 className="text-xl font-bold">Total Exchange Post Users</h2>
+                    <h2 className="text-2xl font-bold text-center">100</h2>
+                </div>
+            </div>
+            <Chart total={200}  number={140} name={name} amount={amount}></Chart>
         </div>
     );
 };
--- src/app/(dashboard)/hivePostReport/page.jsx
+++ src/app/(dashboard)/hivePostReport/page.jsx
@@ -1,10 +1,30 @@
-'use server'
+'use server' 
 import React from 'react';
+import Chart from '@/components/Chart';
+
+
 
 const page = () => {
+
+    const name = 'Total Hive Posts'
+    const amount = 'Total Hive Post Users'
+
     return (
         <div>
-            Hive Post Report
+            <div>
+                <h3 className="text-2xl font-bold text-center p-3 border shadow-md rounded-sm">Hive Posts Report</h3>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-7'>
+                <div className='p-3 border shadow-md rounded-sm hover:shadow-lg hover:scale-105'>
+                    <h2 className="text-xl font-bold">Total Hive Posts</h2>
+                    <h2 className="text-2xl font-bold text-center">600</h2>
+                </div>
+                <div className='p-3 border shadow-md rounded-sm hover:shadow-lg hover:scale-105'>
+                    <h2 className="text-xl font-bold">Total Hive Post Users</h2>
+                    <h2 className="text-2xl font-bold text-center">100</h2>
+                </div>
+            </div>
+            <Chart total={600}  number={100} name={name} amount={amount}></Chart>
         </div>
     );
 };
