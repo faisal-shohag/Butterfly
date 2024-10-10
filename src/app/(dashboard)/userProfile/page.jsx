aba917c53ceb2108b30faf54d88f1d80ab1653c7
Robiu-Sani <hafazrobiussani@gmail.com>
2024-10-10 11:14:16 +0600

    fixing error

--- src/app/(dashboard)/userProfile/page.jsx
+++ src/app/(dashboard)/userProfile/page.jsx
@@ -1,7 +1,6 @@
 "use server";
 import PostSection from "@/app/(routes)/profile/PostSection";
 import UserProfile from "@/app/(routes)/profile/UserProfile";
-import UserSubInformation from "@/app/(routes)/profile/UserSubInformation";
 import { auth } from "@/auth";
 import React from "react";
 
@@ -11,7 +10,7 @@ const page = async () => {
     <div className="mx-auto container p-3 grid grid-cols-1 sm:grid-cols-4 gap-3 mt-[-20px]">
       {/* Main profile section */}
       <div className="w-full col-span-1 sm:col-span-4">
-        {/* Pass userInfo to UserProfile component */}
+        {/* Pass userInfo to UserProfile  component */}
         <UserProfile userInfo={userInfo.user} />
         <PostSection />
       </div>
