50db05442bbef0cdda99e4c4aca5ffe19b64b575
samya-roy <samyaroy999@gmail.com>
2024-10-16 16:06:57 +0600

    deshbord total-count added

--- src/app/dashboard/dashboardHome/TopCards.jsx
+++ src/app/dashboard/dashboardHome/TopCards.jsx
@@ -1,7 +1,6 @@
 "use client"
 import useAxiosSecure from "@/hooks/useAxiosSecure";
-
-import { FaBook, FaUsers, FaRegEnvelope } from "react-icons/fa";
+import { FaBook, FaUsers, FaRegEnvelope, FaFileImage, FaStore  } from "react-icons/fa";
 import { useQuery } from "react-query";
 
 const cardData = [
@@ -35,25 +34,16 @@ const cardData = [
     circleColor: "text-green-500 dark:text-green-400",
     description: "Total active email subscribers",
   },
-  {
-    id: 3,
-    title: "Subscribers",
-    count: "560 Subscribers",
-    progress: 56,
-    icon: <FaRegEnvelope size={28} />,
-    iconBg: "bg-green-500 dark:bg-green-600",
-    circleColor: "text-green-500 dark:text-green-400",
-    description: "Total active email subscribers",
-  },
 ];
 
-
 export default function TopCards() {
+  
   const axiosSecure = useAxiosSecure();
   const {
     data: total,
     isLoading,
     error,
+    refetch
   } = useQuery("total", async () => {
     const response = await axiosSecure.get("/total-count");
     return response.data;
@@ -61,82 +51,93 @@ export default function TopCards() {
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Error loading data</p>;
-console.log(total);
+  refetch()
+  // console.log(total);
 
-  
-return (
-  <div className="grid grid-cols-1 sm:grid-cols-3 gap-3">
-    <h1>{total.totalBooks}</h1>
-    {cardData.map((card) => (
-      <div
-        key={card.id}
-        className="w-full p-6 custom-glass dark:bg-zinc-800 rounded-lg shadow-lg flex items-center justify-between"
-      >
-        <div className="flex flex-col items-start">
-          <div className="flex items-center mb-2">
-            <div
-              className={`p-4 ${card.iconBg} rounded-lg text-white shadow-lg`}
-            >
-              {card.icon}
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-4 gap-3">
+        <div
+          className="w-full p-8  custom-glass dark:bg-zinc-800 rounded-lg shadow-lg "
+        >
+          <div className=" ">
+            <div className="flex items-center mb-2 justify-between w-full">
+              <div className=" w-14 text-center border-4 border-green-600 p-2 rounded-full">
+                <h4 className="font-bold text-3xl text-gray-600 ">{total.totalBooks}</h4>     
+              </div>
+              <div
+                className="p-4 bg-green-500 dark:bg-green-600 rounded-lg text-white shadow-lg"
+              >
+               <FaBook size={28} />
+              </div>
             </div>
-            <div className="ml-4">
-              <h4 className="font-bold text-lg">{card.title}</h4>
-              <p className="text-gray-600 dark:text-gray-400">{card.count}</p>
-            </div>
+            <p className="text-gray-600 font-bold text-l dark:text-gray-400">Total Books</p>
           </div>
-          <span className="text-xs text-gray-500 dark:text-gray-400">
-            {card.description}
-          </span>
         </div>
-        <div className="relative w-20 h-20">
-          <svg className="w-full h-full">
-            <circle
-              className="text-gray-200"
-              strokeWidth="10"
-              stroke="currentColor"
-              fill="transparent"
-              r="35"
-              cx="50%"
-              cy="50%"
-            />
-            <circle
-              className={card.circleColor}
-              strokeWidth="10"
-              strokeDasharray="220"
-              strokeDashoffset={220 - (220 * card.progress) / 100} // Dynamic progress
-              strokeLinecap="round"
-              stroke="currentColor"
-              fill="transparent"
-              r="35"
-              cx="50%"
-              cy="50%"
-            />
-          </svg>
-          <span
-            className={`absolute inset-0 flex items-center justify-center ${card.circleColor} font-bold`}
-          >
-            {card.progress}%
-          </span>
+        <div
+          className="w-full p-8  custom-glass dark:bg-zinc-800 rounded-lg shadow-lg "
+        >
+          <div className=" ">
+            <div className="flex items-center mb-2 justify-between w-full">
+              <div className="w-14 text-center border-4 border-blue-600 p-2 rounded-full">
+                <h4 className="font-bold text-3xl text-gray-600 ">{total.totalPosts}</h4>     
+              </div>
+              <div
+                className="p-4 bg-blue-500 dark:bg-blue-600 rounded-lg text-white shadow-lg"
+              >
+                <FaFileImage size={28}/>
+              </div>
+            </div>
+            <p className="text-gray-600 font-bold text-l dark:text-gray-400">Total Posts</p>
+          </div>
         </div>
-      </div>
-    ))}
-  </div>
-);
+        <div
+          className="w-full p-8  custom-glass dark:bg-zinc-800 rounded-lg shadow-lg "
+        >
+          <div className=" ">
+            <div className="flex items-center mb-2 justify-between w-full">
+              <div className="w-14 text-center border-4 border-purple-600 p-2 rounded-full">
+                <h4 className="font-bold text-3xl text-gray-600 ">{total.totalStoreBooks}</h4>     
+              </div>
+              <div
+                className="p-4 bg-purple-600 dark:bg-purple-600 rounded-lg text-white shadow-lg"
+              >
+                <FaStore  size={28} />
+              </div>
+            </div>
+            <p className="text-gray-600 font-bold text-l dark:text-gray-400">Total Store Books</p>
+          </div>
+        </div>
+        <div
+          className="w-full p-8  custom-glass dark:bg-zinc-800 rounded-lg shadow-lg "
+        >
+          <div className=" ">
+            <div className="flex items-center mb-2 justify-between w-full">
+              <div className="w-14 text-center border-4 border-pink-600 p-2 rounded-full ">
+                <h4 className="font-bold text-3xl  text-gray-600 ">{total.totalUsers}</h4>     
+              </div>
+              <div
+                className="p-4 bg-pink-500 dark:bg-pink-600 rounded-lg text-white shadow-lg"
+              >
+                <FaUsers size={28} />
+              </div>
+            </div>
+            <p className="text-gray-600 font-bold text-l dark:text-gray-400">Total Users</p>
+          </div>
+        </div>
+    </div>
+  );
 }
-
-
 
 
 // return (
 //   <div className="grid grid-cols-1 sm:grid-cols-3 gap-3">
-//     <h1>{total.totalBooks}</h1>
 //     {cardData.map((card) => (
 //       <div
 //         key={card.id}
 //         className="w-full p-6 custom-glass dark:bg-zinc-800 rounded-lg shadow-lg flex items-center justify-between"
 //       >
-//         <div className="flex flex-col items-start">
-//           <div className="flex items-center mb-2">
+//         <div className="flex flex-col ">
+//           <div className="flex items-center mb-2 flex-col">
 //             <div
 //               className={`p-4 ${card.iconBg} rounded-lg text-white shadow-lg`}
 //             >
@@ -151,37 +152,9 @@ return (
 //             {card.description}
 //           </span>
 //         </div>
-//         <div className="relative w-20 h-20">
-//           <svg className="w-full h-full">
-//             <circle
-//               className="text-gray-200"
-//               strokeWidth="10"
-//               stroke="currentColor"
-//               fill="transparent"
-//               r="35"
-//               cx="50%"
-//               cy="50%"
-//             />
-//             <circle
-//               className={card.circleColor}
-//               strokeWidth="10"
-//               strokeDasharray="220"
-//               strokeDashoffset={220 - (220 * card.progress) / 100} // Dynamic progress
-//               strokeLinecap="round"
-//               stroke="currentColor"
-//               fill="transparent"
-//               r="35"
-//               cx="50%"
-//               cy="50%"
-//             />
-//           </svg>
-//           <span
-//             className={`absolute inset-0 flex items-center justify-center ${card.circleColor} font-bold`}
-//           >
-//             {card.progress}%
-//           </span>
-//         </div>
+        
 //       </div>
 //     ))}
 //   </div>
 // );
+// }
