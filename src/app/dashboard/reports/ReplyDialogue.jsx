7bf2d167ec2ba3e9e22d9f98334e4a6742f73d17
Anas Ahammad <anasahammad2002@gmail.com>
2024-10-18 00:10:39 +0600

    added the reports table

---
+++ src/app/dashboard/reports/ReplyDialogue.jsx
@@ -0,0 +1,62 @@
+"use client"
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
+
+const ReplyDialogue = ({createReplyMutation, id, userId, report}) => {
+  const [text, setText] = useState("")
+  
+  const handleReply = ()=>{
+
+    const replyData = {
+      text,
+      userId,
+      report
+    }
+
+    if (!text.trim()) {
+      alert("Reply cannot be empty");
+      return;
+    }
+    createReplyMutation.mutate({ replyData, id });
+    console.log(replyData)
+    setText("")
+  }
+    return (
+        <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Reply</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Reply the Report</DialogTitle>
+          <DialogDescription>
+             Click sent when you're done.
+          </DialogDescription>
+        </DialogHeader>
+       
+         
+          <div className="">
+            {/* <Input id=""  className="" /> */}
+            <Textarea value={text} onChange={(e)=>setText(e.target.value)} placeholder="Write your reply..."/>
+          </div>
+       
+        <DialogFooter>
+          <Button onClick={handleReply}  type="submit">Sent Reply</Button>
+        </DialogFooter>
+       
+      </DialogContent>
+    </Dialog>
+    );
+};
+
+export default ReplyDialogue;
