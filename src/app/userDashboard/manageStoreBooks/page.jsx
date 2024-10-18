8acf29bcd6cfe301b954c6521693e8ab6d4d4ed9
Robiu-Sani <hafazrobiussani@gmail.com>
2024-10-18 11:56:25 +0600

    purchesed book table is done

--- src/app/userDashboard/manageStoreBooks/page.jsx
+++ src/app/userDashboard/manageStoreBooks/page.jsx
@@ -1,5 +1,53 @@
-import React from "react";
+"use client";
+import Loading from "@/components/common/Loading";
+import useAxiosSecure from "@/hooks/useAxiosSecure";
+import { useSession } from "next-auth/react";
+import { useQuery } from "react-query";
+import PurchasedTable from "./PurchasedTable";
 
-export default function page() {
-  return <div>page</div>;
+export default function Page() {
+  const axiosSecure = useAxiosSecure();
+  const { data: session, status } = useSession();
+
+  const id = session?.user?.id;
+
+  const {
+    data: responseData = {},
+    isLoading,
+    isError,
+    refetch,
+  } = useQuery(
+    ["purchesBooks", id],
+    async () => {
+      const response = await axiosSecure.get(`/purchasedBooks/${id}`);
+      return response.data;
+    },
+    {
+      enabled: !!id,
+      keepPreviousData: true,
+    }
+  );
+
+  if (status === "loading" || isLoading) {
+    return <Loading />;
+  }
+
+  if (isError) {
+    return <p className="text-red-500">Failed to load exchange requests.</p>;
+  }
+
+  if (!session) {
+    return <p>Please log in to manage exchange requests.</p>;
+  }
+
+  return (
+    <div className="w-full p-4">
+      <div className="w-full flex justify-center items-center mb-6">
+        <h4 className="font-bold text-2xl text-gray-700 dark:text-white text-center">
+          Manage Store Books
+        </h4>
+      </div>
+      <PurchasedTable purchased={responseData.data} />
+    </div>
+  );
 }
