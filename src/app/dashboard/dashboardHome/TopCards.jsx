ce37cf5b3fdc279ab35828cc006c6dc438cee7b8
samya-roy <samyaroy999@gmail.com>
2024-10-18 14:27:52 +0600

    deshHome top-card added

--- src/app/dashboard/dashboardHome/TopCards.jsx
+++ src/app/dashboard/dashboardHome/TopCards.jsx
@@ -1,93 +1,67 @@
-import { FaBook, FaUsers, FaRegEnvelope } from "react-icons/fa";
-
-const cardData = [
-  {
-    id: 1,
-    title: "Stock Books",
-    count: "1,200 Books",
-    progress: 70,
-    icon: <FaBook size={28} />,
-    iconBg: "bg-blue-500 dark:bg-blue-600",
-    circleColor: "text-blue-500 dark:text-blue-400",
-    description: "Total available stock in inventory",
-  },
-  {
-    id: 2,
-    title: "Total Users",
-    count: "850 Users",
-    progress: 85,
-    icon: <FaUsers size={28} />,
-    iconBg: "bg-purple-500 dark:bg-purple-600",
-    circleColor: "text-purple-500 dark:text-purple-400",
-    description: "Total registered users",
-  },
-  {
-    id: 3,
-    title: "Subscribers",
-    count: "560 Subscribers",
-    progress: 56,
-    icon: <FaRegEnvelope size={28} />,
-    iconBg: "bg-green-500 dark:bg-green-600",
-    circleColor: "text-green-500 dark:text-green-400",
-    description: "Total active email subscribers",
-  },
-];
+"use client";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import useAxiosSecure from "@/hooks/useAxiosSecure";
+import { useQuery } from "react-query";
+import { PiBookOpenTextThin, PiFileImageThin, PiStorefrontThin, PiUsersLight } from "react-icons/pi";
 
 export default function TopCards() {
+
+  const axiosSecure = useAxiosSecure();
+  const {
+    data: total,
+    isLoading,
+    error,
+  } = useQuery("total", async () => {
+    const res = await axiosSecure.get("/total-count");
+    return res.data;
+  });
+
+  if (isLoading) return <p>Loading ...</p>;
+  if (error) return <p>Error loading ...</p>;
+
+  const cardData = [
+    {
+      id: 1,
+      title: "Total Books",
+      icon: <PiBookOpenTextThin size={24} />,
+      totalCounts: total?.totalBooks || 0, 
+    },
+    {
+      id: 2,
+      title: "Total Posts",
+      icon: <PiFileImageThin size={24} />,
+      totalCounts: total?.totalPosts || 0,
+    },
+    {
+      id: 3,
+      title: "Total Store Books",
+      icon: <PiStorefrontThin size={24} />,
+      totalCounts: total?.totalStoreBooks || 0,
+    },
+    {
+      id: 4,
+      title: "Total Users",
+      icon: <PiUsersLight size={24} />,
+      totalCounts: total?.totalUsers || 0,
+    },
+  ];
+
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 gap-3">
-      {cardData.map((card) => (
-        <div
-          key={card.id}
-          className="w-full p-6 custom-glass dark:bg-zinc-800 rounded-lg shadow-lg flex items-center justify-between"
-        >
-          <div className="flex flex-col items-start">
-            <div className="flex items-center mb-2">
-              <div
-                className={`p-4 ${card.iconBg} rounded-lg text-white shadow-lg`}
-              >
-                {card.icon}
-              </div>
-              <div className="ml-4">
-                <h4 className="font-bold text-lg">{card.title}</h4>
-                <p className="text-gray-600 dark:text-gray-400">{card.count}</p>
-              </div>
+    <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+      {cardData.map(item => (
+        <Card key={item.id}>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-lg font-medium">
+              {item.title}
+            </CardTitle>
+            <div className="h-4 w-4 text-muted-foreground">
+              {item.icon}
             </div>
-            <span className="text-xs text-gray-500 dark:text-gray-400">
-              {card.description}
-            </span>
-          </div>
-          <div className="relative w-20 h-20">
-            <svg className="w-full h-full">
-              <circle
-                className="text-gray-200"
-                strokeWidth="10"
-                stroke="currentColor"
-                fill="transparent"
-                r="35"
-                cx="50%"
-                cy="50%"
-              />
-              <circle
-                className={card.circleColor}
-                strokeWidth="10"
-                strokeDasharray="220"
-                strokeDashoffset={220 - (220 * card.progress) / 100} // Dynamic progress
-                strokeLinecap="round"
-                stroke="currentColor"
-                fill="transparent"
-                r="35"
-                cx="50%"
-                cy="50%"
-              />
-            </svg>
-            <span
-              className={`absolute inset-0 flex items-center justify-center ${card.circleColor} font-bold`}
-            >
-              {card.progress}%
-            </span>
-          </div>
-        </div>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">+{item.totalCounts}</div>
+          </CardContent>
+        </Card>
       ))}
     </div>
   );
