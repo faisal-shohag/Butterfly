9e278395d29302e68cd9980afd83bd08d6c03f95
Anas Ahammad <anasahammad2002@gmail.com>
2024-10-22 00:04:34 +0600

    Profile section added

--- src/app/(footer_routes)/sitemap/SitemapRoutes.jsx
+++ src/app/(footer_routes)/sitemap/SitemapRoutes.jsx
@@ -46,6 +46,24 @@ const SitemapRoutes = () => {
                     </ul>        
                         
            </div>
+
+           {/* Profile */}
+            <div>
+           <h3 className="text-[20px]  italic font-bold mb-2 text-[#0A2B34] ">Profile Pages</h3>
+           <hr className="h-[2px] w-1/2 bg-[#0A2B34] "></hr>
+
+                <ul className="mt-6 list-disc pl-6">
+                    <li>
+                        <Link className="hover:text-blue-500" href={"/profile"}>Profile</Link>
+                    </li>
+                    
+                    <li>
+                        <Link className="hover:text-blue-500" href={"/userDashboard/dashboardHome"}>Dashboard</Link>
+                    </li>
+                    
+                    </ul>        
+                        
+           </div>
             
                   
                         
