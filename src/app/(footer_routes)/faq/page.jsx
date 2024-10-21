a0afaea4d10bae9d81922c43276f31614d958c7e
chandro4D <chandroshakhor7@gmail.com>
2024-10-22 02:33:44 +0600

    add feq

--- src/app/(footer_routes)/faq/page.jsx
+++ src/app/(footer_routes)/faq/page.jsx
@@ -1,28 +1,33 @@
 import { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from '@radix-ui/react-accordion';
 
 const FAQ = () => {
-  return (
-    <Accordion type="single" collapsible className="w-full">
-      <AccordionItem value="item-1">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-2">
-        <AccordionTrigger>Is it styled?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It comes with default styles that match the other components' aesthetic.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-3">
-        <AccordionTrigger>Is it animated?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It's animated by default, but you can disable it if you prefer.
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
-  );
+    return (
+        <div className='ml-80 mt-20'>
+            <Accordion type="single" collapsible >
+                <AccordionItem value="item-1">
+                    <div className=" border-t border-gray-200 dark:border-gray-700 w-40"> </div>
+                    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                    
+                    <AccordionContent>
+                        Yes. It adheres to the WAI-ARIA design pattern.
+                    </AccordionContent>
+                    <div className=" border-t border-gray-200 dark:border-gray-700 "> </div>
+                </AccordionItem>
+                <AccordionItem value="item-2">
+                    <AccordionTrigger>Is it styled?</AccordionTrigger>
+                    <AccordionContent>
+                        Yes. It comes with default styles that match the other components' aesthetic.
+                    </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-3">
+                    <AccordionTrigger>Is it animated?</AccordionTrigger>
+                    <AccordionContent>
+                        Yes. It's animated by default, but you can disable it if you prefer.
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
+        </div>
+    );
 };
 
 export default FAQ;
