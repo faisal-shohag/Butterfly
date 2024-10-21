9c160654e6eb44e89b23ab24b6999d3323a37878
Anas Ahammad <anasahammad2002@gmail.com>
2024-10-21 19:39:45 +0600

    cookies sub hading added

--- src/app/(footer_routes)/cookie/page.jsx
+++ src/app/(footer_routes)/cookie/page.jsx
@@ -1,8 +1,14 @@
 
 const CookiePolicy = () => {
     return (
-        <div className="w-full my-7">
+        <div className="w-full my-7 px-6">
             <div className="text-5xl font-thin text-center">COOKIE POLICY</div>
+
+            <div className="font-bold ">Cookies and how we use them</div>
+
+            <div>
+                <h5>What are cookies?</h5>
+            </div>
         </div>
     );
 };
