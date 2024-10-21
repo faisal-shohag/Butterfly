34f6e1afa9cfd84aa143fe931b0776f6811f9add
Anas Ahammad <anasahammad2002@gmail.com>
2024-10-22 00:06:20 +0600

    faq sitemap added

--- src/app/(footer_routes)/sitemap/SitemapRoutes.jsx
+++ src/app/(footer_routes)/sitemap/SitemapRoutes.jsx
@@ -64,6 +64,23 @@ const SitemapRoutes = () => {
                     </ul>        
                         
            </div>
+
+
+           {/* Support */}
+            <div>
+           <h3 className="text-[20px]  italic font-bold mb-2 text-[#0A2B34] ">Support</h3>
+           <hr className="h-[2px] w-1/2 bg-[#0A2B34] "></hr>
+
+                <ul className="mt-6 list-disc pl-6">
+                    <li>
+                        <Link className="hover:text-blue-500" href={"/faq"}>FAQ</Link>
+                    </li>
+                    
+                   
+                    
+                    </ul>        
+                        
+           </div>
             
                   
                         
