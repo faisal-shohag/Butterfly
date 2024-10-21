24514ab4642063379cbe8bb5ca782aab9dd34c8c
chandro4D <chandroshakhor7@gmail.com>
2024-10-22 03:06:38 +0600

    update friquently asked question

--- src/app/(footer_routes)/faq/page.jsx
+++ src/app/(footer_routes)/faq/page.jsx
@@ -2,23 +2,44 @@ import { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from '@r
 
 const FAQ = () => {
     return (
-        <div className='ml-80 mt-20'>
-            <div>
-                <h1>HAVE A QUESTION? START HERE</h1>
-                <h3>Answers to out most frequently asked questions</h3>
+        <div className=' mt-20 mb-20'>
+            <div className='ml-[460px]'>
+                <h1 className='font-medium text-4xl pb-2'>HAVE A QUESTION? START HERE</h1>
+                <h3 className='font-light text-xl'>Answers to out most frequently asked questions</h3>
             </div>
-            <Accordion type="single" collapsible >
-                <AccordionItem value="item-1">
-                    <div className=" border-t border-gray-200 dark:border-gray-700 w-[600px]"> </div>
-                    <AccordionTrigger className='py-3'>Is it accessible?</AccordionTrigger>
+            <div className='justify-items-center'>
+                <Accordion type="single" collapsible >
+                    <AccordionItem value="item-1">
+                        <div className=" border-t border-gray-200 dark:border-gray-700 w-[600px] mt-3"> </div>
+                        <AccordionTrigger className='py-3'>Is it accessible?</AccordionTrigger>
+                        <AccordionContent>
+                            
+                            Yes. It adheres to the WAI-ARIA design pattern.
+                        </AccordionContent>
 
-                    <AccordionContent>
-                        Yes. It adheres to the WAI-ARIA design pattern.
-                    </AccordionContent>
-                    <div className=" border-t border-gray-200 dark:border-gray-700 w-[600px]"> </div>
-                </AccordionItem>
-                
-            </Accordion>
+                    </AccordionItem>
+                </Accordion>
+                <Accordion type="single" collapsible >
+                    <AccordionItem value="item-1">
+                        <div className=" border-t border-gray-200 dark:border-gray-700 w-[600px] mt-3"> </div>
+                        <AccordionTrigger className='py-3'>Is it accessible?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It adheres to the WAI-ARIA design pattern.
+                        </AccordionContent>
+
+                    </AccordionItem>
+                </Accordion>
+                <Accordion type="single" collapsible >
+                    <AccordionItem value="item-1">
+                        <div className=" border-t border-gray-200 dark:border-gray-700 w-[600px] mt-3"> </div>
+                        <AccordionTrigger className='py-3'>Is it accessible?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It adheres to the WAI-ARIA design pattern.
+                        </AccordionContent>
+                        <div className=" border-t border-gray-200 dark:border-gray-700 w-[600px]"> </div>
+                    </AccordionItem>
+                </Accordion>
+            </div>
         </div>
     );
 };
