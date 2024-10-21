a6ef19b8b312063d676a7d8be2becb7471f59395
Anas Ahammad <anasahammad2002@gmail.com>
2024-10-21 23:27:13 +0600

    get to know us added

--- src/app/(footer_routes)/sitemap/SitemapRoutes.jsx
+++ src/app/(footer_routes)/sitemap/SitemapRoutes.jsx
@@ -4,11 +4,19 @@ import SitemapList from "./SitemapList";
 
 const SitemapRoutes = () => {
     return (
-        <div className="w-full px-40">
+        <div className="w-full px-40 my-16">
             <SitemapList>
-           <h3 className="text-[20px] italic font-bold mb-2 text-[#0A2B34] ">Shop Categories</h3>
+           <h3 className="text-[20px] italic font-bold mb-2 text-[#0A2B34] ">Get to know us</h3>
            <hr className="h-[2px] w-1/2 bg-[#0A2B34] "></hr>
-                        
+
+                <ul className="mt-6 list-disc pl-6">
+                    <li>
+                        <Link href={"/contact"}>Contact us</Link>
+                    </li>
+                    <li>
+                        <Link href={"/about"}>About us</Link>
+                    </li>
+                    </ul>        
                         
            </SitemapList>
         </div>
