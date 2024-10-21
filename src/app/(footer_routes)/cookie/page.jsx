cefd9a416cb77d860f18d256a4470db6a332f28a
Anas Ahammad <anasahammad2002@gmail.com>
2024-10-21 19:50:54 +0600

    what are cookies added

--- src/app/(footer_routes)/cookie/page.jsx
+++ src/app/(footer_routes)/cookie/page.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 const CookiePolicy = () => {
     return (
@@ -7,7 +8,10 @@ const CookiePolicy = () => {
             <div className="font-bold ">Cookies and how we use them</div>
 
             <div>
-                <h5>What are cookies?</h5>
+                <h5 className="font-bold text-sm">What are cookies?</h5>
+                <p className="text-[13px]">Cookies are text files containing small amounts of information which are downloaded to your device when you visit a website. Cookies are then sent back to the originating website on each subsequent visit, or to another website that recognises that cookie. Cookies are useful because they allow a website to recognise a user’s device.</p>
+
+                <p className="text-[13px]">You can find more information about cookies at: <Link className="underline" target="_blank" href={"www.allaboutcookies.org"}>www.allaboutcookies.org</Link> and <Link className="underline" target="_blank" href="www.youronlinechoices.eu">www.youronlinechoices.eu</Link>.</p>
             </div>
         </div>
     );
