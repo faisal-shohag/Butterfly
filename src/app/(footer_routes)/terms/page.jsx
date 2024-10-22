148e36e55a3b496269146e2b4d1a98d0b2541971
Masudur400 <md8004210@gmail.com>
2024-10-22 11:31:56 +0600

    Acceptance of Terms added

--- src/app/(footer_routes)/terms/page.jsx
+++ src/app/(footer_routes)/terms/page.jsx
@@ -4,6 +4,8 @@ const Terms = () => {
         <div>
              <h2 className="text-2xl font-bold">ButterFly Terms and Service</h2>
             <p> Welcome to Butterfly Books! We are delighted to provide a platform where users can exchange, sell, and buy books. By using our services, you agree to the following terms and conditions. Please review them carefully to understand your rights and responsibilities.</p>
+            <h2 className="text-2xl font-bold">1. Acceptance of Terms</h2>
+            <p>By accessing or using Butterfly Books, you agree to be bound by these terms. If you do not agree with any part of these terms, you should not use the platform.</p>
         </div>
     );
 };
