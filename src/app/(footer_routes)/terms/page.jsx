1e1a4a9a380211db69163b8da0a245e6a3307a21
Masudur400 <md8004210@gmail.com>
2024-10-22 11:36:22 +0600

    Account Responsibilities added

--- src/app/(footer_routes)/terms/page.jsx
+++ src/app/(footer_routes)/terms/page.jsx
@@ -4,8 +4,18 @@ const Terms = () => {
         <div>
              <h2 className="text-2xl font-bold">ButterFly Terms and Service</h2>
             <p> Welcome to Butterfly Books! We are delighted to provide a platform where users can exchange, sell, and buy books. By using our services, you agree to the following terms and conditions. Please review them carefully to understand your rights and responsibilities.</p>
+
             <h2 className="text-2xl font-bold">1. Acceptance of Terms</h2>
             <p>By accessing or using Butterfly Books, you agree to be bound by these terms. If you do not agree with any part of these terms, you should not use the platform.</p>
+
+            <h2 className="text-2xl font-bold">2. Account Responsibilities</h2>
+            <p>
+                <ul class="list-disc pl-5">
+                    <li>You are responsible for the accuracy and security of your account information.</li>
+                    <li>Make sure to keep your account information updated to ensure a smooth experience.</li>
+                    <li>We encourage open and honest participation within the community and appreciate your cooperation in maintaining a positive atmosphere.</li>
+                </ul>
+            </p>
         </div>
     );
 };
