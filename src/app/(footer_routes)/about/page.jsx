d311bef66ef9db4b65258dc94b7b9b1a2fd7bd9a
samya-roy <samyaroy999@gmail.com>
2024-10-22 13:59:28 +0600

    About us for banner img added

--- src/app/(footer_routes)/about/page.jsx
+++ src/app/(footer_routes)/about/page.jsx
@@ -1,8 +1,15 @@
 
 const About = () => {
     return (
-        <div>
-            About
+        <div className='max-w-screen-lg mx-auto'>
+            <Card
+                className="lg:h-[400px] md:h-[300px] h-[300px] relative rounded-xl bg-fixed bg-cover bg-center"
+                style={{
+                    backgroundImage: `url('https://i.postimg.cc/hjzcgJcs/araix-rand-Xe46k-NRh-Xs-A-unsplash.jpg')`,
+                }}
+            >
+                
+            </Card>
         </div>
     );
 };
