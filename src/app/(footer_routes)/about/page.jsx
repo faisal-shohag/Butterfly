daabffb8cad8368bb4a9a10a373e92fe39be1cd8
samya-roy <samyaroy999@gmail.com>
2024-10-22 14:12:05 +0600

    our Mission And Values aded

--- src/app/(footer_routes)/about/page.jsx
+++ src/app/(footer_routes)/about/page.jsx
@@ -1,4 +1,7 @@
 
+import { Card, CardDescription, CardTitle } from '@/components/ui/card'
+import React from 'react'
+
 const About = () => {
     return (
         <div className='max-w-screen-lg mx-auto'>
@@ -17,6 +20,15 @@ const About = () => {
                         At Butterfly, our mission is to bring book lovers together by creating a platform for seamless <br /> book exchanges. We believe that every book deserves a second life, and  through our platform, we aim to foster <br /> a community where knowledge and stories can be shared freely and sustainably.
                     </CardDescription>
                 </div>
+            </Card>
+            
+            <Card className="text-center py-12 px-4">
+                <CardTitle className="text-2xl font-bold">Mission And Values</CardTitle>
+                <CardDescription className="mt-4 text-gray-700 max-w-2xl mx-auto">
+                    Our mission is to create a vibrant community of book lovers who can easily exchange and share their favorite reads, promoting sustainability and a love for literature. We value accessibility, community engagement, and the belief that every book can bring joy and insight to someone else.
+                </CardDescription>
+
+               
             </Card>
         </div>
     );
