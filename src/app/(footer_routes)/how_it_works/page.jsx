1171a52308fb5ae6f71c77b3679a95e213e52904
Masudur400 <md8004210@gmail.com>
2024-10-23 11:24:14 +0600

    Post Your Books for Exchange or Sale

--- src/app/(footer_routes)/how_it_works/page.jsx
+++ src/app/(footer_routes)/how_it_works/page.jsx
@@ -4,6 +4,7 @@ const HowItWorks = () => {
         <div className="px-3 space-y-2">
             <h2 className="text-2xl font-bold opacity-75">How ButterFly Work</h2>
             <p> Welcome to Butterfly Books, where book lovers can exchange and buy books from one another. Whether you are here to give your books a new home or find your next great read, we make the process simple and enjoyable. Here's how it works: </p>
+
             <h2 className="text-xl font-bold opacity-75">1. Create an Account</h2>
             <p>To start exchanging or buying books, you need to sign up for an account. It's free and quick! Once you have registered, you will have access to all the features of the platform, including posting books for exchange or sale.</p>
              
@@ -14,6 +15,8 @@ const HowItWorks = () => {
                 <li>You’re all set to explore the platform.</li>
             </ul>
              
+            <h2 className="text-xl font-bold opacity-75">2. Post Your Books for Exchange or Sale</h2>
+            <p>Have books you’d like to give a new home? Post them on our platform so others can find and request them.</p>
 
         </div>
     );
