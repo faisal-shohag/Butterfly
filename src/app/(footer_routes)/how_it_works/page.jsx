83cadd60e23a74be674d170d0511d870a0a68f4e
Masudur400 <md8004210@gmail.com>
2024-10-23 11:17:43 +0600

    How ButterFly Work added

--- src/app/(footer_routes)/how_it_works/page.jsx
+++ src/app/(footer_routes)/how_it_works/page.jsx
@@ -2,7 +2,8 @@
 const HowItWorks = () => {
     return (
         <div>
-            How It Works
+            <h2 className="text-2xl font-bold opacity-75">How ButterFly Work</h2>
+            <p> Welcome to Butterfly Books, where book lovers can exchange and buy books from one another. Whether you are here to give your books a new home or find your next great read, we make the process simple and enjoyable. Here's how it works: </p>
         </div>
     );
 };
