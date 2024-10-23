107fafbb4d02037b6db5eb2afc05da35733a5b08
Masudur400 <md8004210@gmail.com>
2024-10-23 11:23:02 +0600

    padding and space added

--- src/app/(footer_routes)/how_it_works/page.jsx
+++ src/app/(footer_routes)/how_it_works/page.jsx
@@ -1,7 +1,7 @@
 
 const HowItWorks = () => {
     return (
-        <div>
+        <div className="px-3 space-y-2">
             <h2 className="text-2xl font-bold opacity-75">How ButterFly Work</h2>
             <p> Welcome to Butterfly Books, where book lovers can exchange and buy books from one another. Whether you are here to give your books a new home or find your next great read, we make the process simple and enjoyable. Here's how it works: </p>
             <h2 className="text-xl font-bold opacity-75">1. Create an Account</h2>
