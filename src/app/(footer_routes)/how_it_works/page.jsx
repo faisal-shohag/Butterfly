477b470f74ca57895ba90522a098fc7912de61c6
Masudur400 <md8004210@gmail.com>
2024-10-23 11:19:11 +0600

    how to Create an Account

--- src/app/(footer_routes)/how_it_works/page.jsx
+++ src/app/(footer_routes)/how_it_works/page.jsx
@@ -4,6 +4,9 @@ const HowItWorks = () => {
         <div>
             <h2 className="text-2xl font-bold opacity-75">How ButterFly Work</h2>
             <p> Welcome to Butterfly Books, where book lovers can exchange and buy books from one another. Whether you are here to give your books a new home or find your next great read, we make the process simple and enjoyable. Here's how it works: </p>
+            <h2 className="text-2xl font-bold opacity-75">1. Create an Account</h2>
+            <p>To start exchanging or buying books, you need to sign up for an account. It's free and quick! Once you have registered, you will have access to all the features of the platform, including posting books for exchange or sale.</p>
+
         </div>
     );
 };
