c95e13327543464a335300961d0a4947ceca3e53
Robiu-Sani <hafazrobiussani@gmail.com>
2024-10-25 01:10:36 +0600

    userdashboard is done

--- src/app/userDashboard/dashboardHome/page.jsx
+++ src/app/userDashboard/dashboardHome/page.jsx
@@ -1,8 +1,9 @@
 "use server";
 
 import UserTopCards from "./UserTopCards";
-import UserDashboardChart from "./UserDashboardChart";
-import CalenderComponent from "./CalenderComponent";
+import { DashboardLineChart } from "@/app/dashboard/dashboardHome/DashboardLineChart";
+import { DashboardPieChart } from "@/app/dashboard/dashboardHome/DashboardPieChart";
+import BookTable from "./BookTable";
 
 const page = () => {
   return (
@@ -13,13 +14,15 @@ const page = () => {
       </div>
       <UserTopCards />
       <div className="w-full grid grid-cols-1 sm:grid-cols-3 gap-4 my-4">
-        <div className="w-full col-span-2">
-          <UserDashboardChart />
+        <div className="w-full col-span-1 sm:col-span-2">
+          <DashboardLineChart />
         </div>
         <div className="w-full">
-          <CalenderComponent />
+          <DashboardPieChart />
         </div>
       </div>
+      <h4 className="font-bold text-gray-500">Your favorite books</h4>
+      <BookTable />
     </div>
   );
 };
