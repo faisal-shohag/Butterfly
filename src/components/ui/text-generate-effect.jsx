5cf6b4e4a0c54003b46a09d3da6814eac7ab2f9b
faisal-shohag <faisalshohagprog@gmail.com>
2024-10-31 02:17:07 +0600

    added new page restore

--- src/components/ui/text-generate-effect.jsx
+++ src/components/ui/text-generate-effect.jsx
@@ -41,7 +41,7 @@ export const TextGenerateEffect = ({
   };
 
   return (
-    (<div className={cn("font-bold", className)}>
+    (<div className={cn("", className)}>
       <div className="mt-4">
         <div
           className=" dark:text-white text-black leading-snug tracking-wide">
