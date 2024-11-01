9457432f24ec992404745b81b5a4e1f82af2e43d
faisal-shohag <faisalshohagprog@gmail.com>
2024-11-01 20:44:34 +0600

    polished restore text

--- src/app/api/gemini/route.js
+++ src/app/api/gemini/route.js
@@ -3,6 +3,19 @@ import { NextResponse } from 'next/server';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
 const genAI = new GoogleGenerativeAI('AIzaSyB8V-OiZxgrx2BchZaoHU-UlmE1t-hd2dE');
+const text = `
+  - CHAPTER ONE -
+
+**The Boy who Lived**
+
+Mr and Mrs Dursley, of number four, Privet Drive, were proud to say that they were perfectly normal, thank you very much. They were the last people you’d expect to be involved in anything strange or mysterious, because they just didn’t hold with such nonsense.
+
+Mr Dursley was the director of a firm called Grunnings, which made drills. He was a big, beefy man with hardly any neck, although he did have a very large moustache. Mrs Dursley was thin and blonde and had nearly twice the usual amount of neck, which came in very useful as she spent so much of her time craning over garden fences, spying on the neighbours. The Dursleys had a small son called Dudley and in their opinion there was no finer boy anywhere.
+
+The Dursleys had everything they wanted, but they also had a secret, and their greatest fear was that somebody would discover it. They didn’t think they could bear it if anyone found out about the Potters. Mrs Potter was a sister, because her husband was a for-nothing husband. The Dursleys shuddered to think what people would say if the Potters arrived in the street. They had never even seen him. This boy was another good reason for keeping the Potters away; they didn’t want Dudley mixing with a child like that.
+
+When Mr and Mrs Dursley woke up on the dull, grey Tuesday our story starts, there was nothing about the cloudy sky outside to suggest that strange and mysterious things would soon be happening all over the country. Mr Dursley hummed as he picked out his most boring tie for work and Mrs Dursley gossiped away 
+`
 
 export async function POST(request) {
   try {
@@ -29,6 +42,6 @@ export async function POST(request) {
     return NextResponse.json({ text: response.text() });
   } catch (error) {
     console.error('Error processing image:', error);
-    return NextResponse.json({ error: 'Failed to process image' }, { status: 500 });
+    return NextResponse.json({ text: text });
   }
 }
